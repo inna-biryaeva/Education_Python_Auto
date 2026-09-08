d084e97ec528641ccfe98ae3f0524bc0226f9d2b
inna-biryaeva <biryaeva-inna@yandex.ru>
2026-09-09 00:24:48 +0400

    Add Lesson_02

---
+++ Lesson_02/Lesson_2_task_6.py
@@ -0,0 +1,4 @@
+lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
+for number in lst:
+    if number % 3 == 0 and number < 30:
+        print(number)
---
+++ Lesson_02/Lesson_2_task_7.py
@@ -0,0 +1,2 @@
+sequence = list(range(18, -2, -4))
+print(sequence)
